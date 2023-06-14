6b00e1269114326f05711824fa8b940b8df3cd3a
Pui Ho Wilson Tsang <tsangphwilson@gmail.com>
2023-06-14 06:33:56 +0000

    Version 2.0

--- circular_encrypt.py
+++ circular_encrypt.py
@@ -1,1 +1,20 @@
+def main():
+    """
+    Decode circularly encrypted texts.
+    """
+    input_str = input('Encrypted text: ')
+    for i in range(1,3):
+        output_str = ''
+        for j in input_str:
+            ordj = ord(j)
+            if ordj >= 65 and ordj <= 90:
+                output_str += chr(ordj+i)
+            elif ordj >= 97 and ordj <= 122:
+                output_str += chr(ordj+i)
+            else:
+                output_str += j
 
+        print(i,output_str)
+
+
+main()
